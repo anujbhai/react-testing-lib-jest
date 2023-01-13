6532ee4e285704dcfa10ad24e3aac013aa97c105
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-01-13 23:57:27 +0530

    test-01-crash-course - real world example and jest mosk async data w/ axios.

--- react-testing-lib-jest/src/App.js
+++ react-testing-lib-jest/src/App.js
@@ -1,33 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
-  let a = 2;
-  let b = 2;
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          <li>Apple</li>
-          <li>Orange</li>
-          <li>Banana</li>
-        </ul>
-        <h2 data-testid="mytestid">Hello</h2>
-        <span title="sum">{a + b}</span>
-      </header>
+      <Login />
     </div>
   );
 }
---
+++ react-testing-lib-jest/src/components/Login/index.jsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    try {
+      const {data} = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+      setUser(data);
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <span className="user">{user.name}</span>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          disabled={!username || !password}
+          onClick={handleClick}
+        >{loading ? "Please wait" : "Login"}</button>
+        <span
+          data-testid="error"
+          style={{visibility: error? "visible" : "hidden"}}
+        >
+          Something went wrong
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
+
