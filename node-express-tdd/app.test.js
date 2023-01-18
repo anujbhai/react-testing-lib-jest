7e2beb322a96ae83331e514265576bc3c6cf5691
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-01-18 23:59:14 +0530

    test-02-node-express-api-tdd - listing and single routes testing.

--- node-express-tdd/app.test.js
+++ node-express-tdd/app.test.js
@@ -2,29 +2,34 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('Todos', () => {
-  it('should return an array of todos', async () => {
-    const res = await request(app).get('./todos')
+  it('should return an array of todos', () => {
+    return request(app).get('/todos')
       .expect('Content-Type', /json/)
-      .expect(200);
-    expect(res.body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          name: expect.any(String),
-          completed: expect.any(Boolean),
-        }),
-      ]));
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(Number),
+              name: expect.any(String),
+              completed: expect.any(Boolean),
+            }),
+          ])
+        );
+      });
   });
 
-  it('should return specific todo by id', async () => {
-    const todoArr = request(app).get('./todos/1');
-
-    const res = await todoArr
+  it('should return specific todo by id', () => {
+    return request(app).get('/todos/1')
       .expect('Content-Type', /json/)
-      .expect(200);
-    expect(res.body).toEqual({
-      name: expect.any(String),
-      completed: expect.any(Boolean),
-    });
+      .expect(200)
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(Number),
+          name: expect.any(String),
+          completed: expect.any(Boolean),
+        });
+      });
   });
 
   it('should return 404 not found', () => {
@@ -32,23 +37,26 @@ describe('Todos', () => {
   });
 
   it('should create a new todo', async () => {
-    const res = await request(app)
+    return request(app)
       .post('/todos')
       .send({
         name: 'do dishes'
       })
-      .expect('Content-Type', '/json/')
-      .expect(201);
-    expect(res.body).toEqual(
-      expect.objectContaining({
-        name: 'do dishes',
-        completed: false
-      })
-    );
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then(res => {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            name: 'do dishes',
+            completed: false
+          })
+        );
+      });
   });
 
   it('should validate request body', () => {
     return request(app).post('/todos').send({name: 123}).expect(422);
   });
 });
+
 
--- node-express-tdd/routes/todos.js
+++ node-express-tdd/routes/todos.js
@@ -5,7 +5,12 @@ const todos = [{id: 1, name: 'Do something', completed: false}];
 
 /* GET todos listing. */
 router.get('/', function(req, res, next) {
-  res.json('respond with a resource');
+  res.json(todos);
+});
+
+router.get('/:id', function(req, res, next) {
+  const foundTodo = todos.find(todo => todo.id === Number(req.params.id));
+  res.json(foundTodo);
 });
 
 module.exports = router;
